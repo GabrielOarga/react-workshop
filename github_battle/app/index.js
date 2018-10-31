93489daea82c4c35de0c0cd0b039fe0887b92ef7
GabrielOarga <13384992+GabrielOarga@users.noreply.github.com>
2018-10-31 11:49:44 +0200

    added firebase config and scripts

--- github_battle/app/index.js
+++ github_battle/app/index.js
@@ -1,6 +1,7 @@
 var React  = require('react');
 var ReactDOM = require('react-dom');
-require('./index.css');
+var RTChart = require ('react-rt-chart');
+//require('./index.css');
 
 // Separation of concerns, not of technologies
 
