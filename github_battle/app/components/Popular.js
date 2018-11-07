0dfd9a3f8a0abceab5dd76b5fec112d94aeee45a
GabrielOarga <13384992+GabrielOarga@users.noreply.github.com>
2018-11-07 13:49:18 +0200

    Completed Popular module

--- github_battle/app/components/Popular.js
+++ github_battle/app/components/Popular.js
@@ -1,39 +1,50 @@
 var React = require('react');
 
 class Popular extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {
-            selectedLanguage: 'All'
-        }
-
-        //Binds the 'this' keyword of the updateLanguage function
-        //to the context of the Popular object.
-        this.updateLanguage = this.updateLanguage.bind(this);
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All'
     }
 
-    updateLanguage(lang) {
-        this.setState(function () {
-            return {
-                selectedLanguage: lang
-            }
-        })
-    }
+    //Binds the 'this' keyword of the updateLanguage function
+    //to the context of the Popular object.
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
 
-    render() {
-        var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-        return (
-            <ul className='languages'>
-                {languages.map(function (lang) { 
-                    return (
-                        <li key={lang} onClick={this.updateLanguage}>
-                            {lang}
-                        </li>
-                    )
-                }, this /*context to be used within the callback function*/)}
-            </ul>
-        )
-    }
+  updateLanguage(lang) {
+    this.setState(function () {
+      return {
+        selectedLanguage: lang
+      }
+    })
+  }
+
+  render() {
+    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+    return (
+      <ul className='languages'>
+        {languages.map((lang) => { /* Arrow function preserves 'this' */
+          return (
+            <li
+              style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}
+              key={lang}
+              onClick={
+                /*
+                    This will create a new updateLanguage function and pass it
+                    a context 'null' (because we already did it in the constructor)
+                    and a parameter 'lang'
+                */
+                this.updateLanguage.bind(null, lang)
+              }
+            >
+              {lang}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
 }
 
 module.exports = Popular;
