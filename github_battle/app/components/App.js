9048621b4d19221a4529afe58b9f238ab1cf2347
GabrielOarga <13384992+GabrielOarga@users.noreply.github.com>
2018-11-07 15:46:58 +0200

    Refactored Popular by extracting SelectLanguage as it's own stateless functional component

--- github_battle/app/components/App.js
+++ github_battle/app/components/App.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var Popular = require('./Popular');
+var Test = require('./Test');
 
 class App extends React.Component {
     render() {
         return (
             <div className='container'>
                 <Popular />
+                <Test text='Cucu'/>
             </div>
         )
     }
--- github_battle/app/components/Popular.js
+++ github_battle/app/components/Popular.js
@@ -1,4 +1,37 @@
 var React = require('react');
+var PropTypes = require('prop-types')
+
+function SelectLanguage (props) { /*Stateless functional component*/
+  var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
+  return (
+    <ul className='languages'>
+      {languages.map((lang) => { /* Arrow function preserves 'this' */
+        return (
+          <li
+            style={lang === props.selectedLanguage ? { color: '#d0021b' } : null}
+            key={lang}
+            onClick={
+              /*
+                  This will create a new updateLanguage function and pass it
+                  a context 'null' (because we already did it in the constructor)
+                  and a parameter 'lang'
+              */
+              props.onSelect.bind(null, lang)
+            }
+          >
+            {lang}
+          </li>
+        )
+      })}
+  </ul>
+  )
+}
+
+SelectLanguage.propTypes = {
+  selectedLanguage: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
 
 class Popular extends React.Component {
   constructor(props) {
@@ -21,28 +54,13 @@ class Popular extends React.Component {
   }
 
   render() {
-    var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
     return (
-      <ul className='languages'>
-        {languages.map((lang) => { /* Arrow function preserves 'this' */
-          return (
-            <li
-              style={lang === this.state.selectedLanguage ? { color: '#d0021b' } : null}
-              key={lang}
-              onClick={
-                /*
-                    This will create a new updateLanguage function and pass it
-                    a context 'null' (because we already did it in the constructor)
-                    and a parameter 'lang'
-                */
-                this.updateLanguage.bind(null, lang)
-              }
-            >
-              {lang}
-            </li>
-          )
-        })}
-      </ul>
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+      </div>
     )
   }
 }
