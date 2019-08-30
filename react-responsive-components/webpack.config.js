3a9b27f52b7a2e4a00300be2124dcc77f4284c78
GabrielOarga <13384992+GabrielOarga@users.noreply.github.com>
2019-08-30 15:56:41 +0300

    Project setup compelte

---
+++ react-responsive-components/webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './build',
+    publicPath: '/dist/',
+    open: 'Google Chrome'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader']
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
+      }
+    ]
+  },
+};
